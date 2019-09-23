084c88de931d7fa7ead4e4908098fda38958b18a
Aslem <agathe_79@hotmail.fr>
2019-09-23 23:10:40 +0200

    Ajout facture.py

---
+++ facture.py
@@ -0,0 +1,9 @@
+a = input ("Designation ?")
+b = input ("Prix unitaire ?")
+c = input ("Quantité ?")
+
+'''
+Un indice, toi que j'aime,il faut re-convertir le calcul en str. ^^ Pour ça, il faut mettre la fonction str() quelque part. ^^
+Pleins de bizzzouxxx !!
+'''
+print ("facture : "+ c, a +"à" +b+ "€ l'unité font "+ float (c)*float (b)+ "€" )
