2e77f8a61c59f40052fab63d18b48f68d2286680
Aslem <agathe_79@hotmail.fr>
2019-10-20 18:07:40 +0200

    exercice

--- Exercices (python)/année_bissextile.py
+++ Exercices (python)/année_bissextile.py
@@ -37,3 +37,16 @@ if bissextile: # Si l'année est bissextile
     print("L'année saisie est bissextile.")
 else:
     print("L'année saisie n'est pas bissextile.")
+
+
+print ("-----------------------------------------------------------------------------------------")
+
+
+#Autre correction possible
+annee = input("Saisissez une année : ") # On attend que l'utilisateur saisisse l'année qu'il désire tester
+annee = int(annee) # Risque d'erreur si l'utilisateur n'a pas saisi un nombre
+
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
+    print("L'année saisie est bissextile.")
+else:
+    print("L'année saisie n'est pas bissextile.")
