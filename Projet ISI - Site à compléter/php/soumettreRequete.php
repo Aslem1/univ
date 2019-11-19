513c4542b7ebf32ec4a51f5ffacb34b47dad2209
Aslem1 <55717071+Aslem1@users.noreply.github.com>
2019-11-19 14:22:40 +0100

    Add files via upload

--- Projet ISI - Site à compléter/php/soumettreRequete.php
+++ Projet ISI - Site à compléter/php/soumettreRequete.php
@@ -1,58 +1,58 @@
 <?php
 
 // En local avec easyphp
-// define("base_de_donnees","tea_isi" ) ;
-// define("login", "root") ;
-// define("mot_de_passe", "") ;
+define("base_de_donnees","L1ISI2019126db" ) ;
+define("L1ISI2019126", "root") ;
+define("14ab402d", "") ;
 
 // Sur le serveur ftpueweb
-// define("base_de_donnees","placez ici le nom de votre base de données sur le serveur ftpueweb" ) ;
-// define("login", "placez ici votre login pour l'accès à la base de données") ;
-// define("mot_de_passe", "placez ici votre mot de passe pour l'accèsà la base de données") ;
+define("base_de_donnees","placez ici le nom de votre base de données sur le serveur ftpueweb" ) ;
+define("login", "placez ici votre login pour l'accès à la base de données") ;
+define("mot_de_passe", "placez ici votre mot de passe pour l'accèsà la base de données") ;
 
 
- 
+
 try {  // Tentative de connexion
-        $acces_BD = "mysql:dbname=".base_de_donnees.";host=localhost" ;
-        // Création d'un objet de connexion à la BD
-        $db = new PDO($acces_BD, login, mot_de_passe);
-    } 
-    catch(PDOException $e) // Si la connexion ne fonctionne pas ...
-    {
-        $db = null;
-        echo    '<p>La connexion a échoué :-( : </p>' ;
-        echo    '<p>Voici le message d\'erreur généré : '. $e->getMessage();
-        exit() ;
-    }
+    $acces_BD = "mysql:dbname=".base_de_donnees.";host=localhost" ;
+    // Création d'un objet de connexion à la BD
+    $db = new PDO($acces_BD, login, mot_de_passe);
+} 
+catch(PDOException $e) // Si la connexion ne fonctionne pas ...
+{
+    $db = null;
+    echo    '<p>La connexion a échoué :-( : </p>' ;
+    echo    '<p>Voici le message d\'erreur généré : '. $e->getMessage();
+    exit() ;
+}
 
-    // Indique que le texte des requêtes sera codé au format UTF8
-    $db->query("SET NAMES utf8");
-    
-    // Récupère le texte de la requête
-    $texteRequete = $_POST['texteRequete'] ;
+// Indique que le texte des requêtes sera codé au format UTF8
+$db->query("SET NAMES utf8");
+
+// Récupère le texte de la requête
+$texteRequete = $_POST['texteRequete'] ;
 
 // Pour les tests
-    //    $texteRequete = $_GET['texteRequete'] ;
-    //    echo $texteRequete ;
-    // Prépare la requête
-    $requete = $db->prepare($texteRequete);
-    
-    // Exécute la requête
-    $requete->execute() ;
+//    $texteRequete = $_GET['texteRequete'] ;
+//    echo $texteRequete ;
+// Prépare la requête
+$requete = $db->prepare($texteRequete);
 
-    // Récupère les résultats de la requête
-    $personnes = $requete->fetchAll(PDO::FETCH_OBJ) ;
+// Exécute la requête
+$requete->execute() ;
 
-    // Pour les tests, affichage pour le développeur
-     //var_dump($personnes) ;
-    
-    // Transformation du résultat en un texte JSON
-    $resultatJson = json_encode($personnes);     
-   
-    // Permettre l'accès à ce fichier depuis n'importe quel serveur
-   header("Access-Control-Allow-Origin: *");
-   
-   // Avertir celui qui reçoit le résultat, que c'est un texte JSON codé en UTF8
-       header('Content-type: application/json; charset=UTF-8');  
+// Récupère les résultats de la requête
+$personnes = $requete->fetchAll(PDO::FETCH_OBJ) ;
+
+// Pour les tests, affichage pour le développeur
+//var_dump($personnes) ;
+
+// Transformation du résultat en un texte JSON
+$resultatJson = json_encode($personnes);     
+
+// Permettre l'accès à ce fichier depuis n'importe quel serveur
+header("Access-Control-Allow-Origin: *");
+
+// Avertir celui qui reçoit le résultat, que c'est un texte JSON codé en UTF8
+header('Content-type: application/json; charset=UTF-8');  
 ?>
 <?=$resultatJson?> 
