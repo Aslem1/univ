57ab4fef2013e7ac57eef79d9e56735dff44b355
Aslem1 <55717071+Aslem1@users.noreply.github.com>
2019-11-25 09:54:43 +0100

    Add files via upload

--- Python/turtle_liste.py
+++ Python/turtle_liste.py
@@ -2,7 +2,9 @@
 from turtle import *
 
 def rect (x, y, h, l=30):
+    penup()
     setposition(x,y)
+    pendown()
     forward (l)
     left (90)
     forward (h)
@@ -17,9 +19,12 @@ rect(0, 0, 100, 30)
 rect(30, 0, 180, 30)
 rect(60, 0, 50, 30)
 
-def histo(Liste, largueur):
-    for l in Liste:
-        rect (...)
+def histo(Liste, largeur, x0=0, y0=0):
+    for l in enumerate (Liste):
+        rect (x0 + i*largeur, y0, x, l=largeur)
+
+clear()
+histo([100,50,-20,200,10,30])
 
 #Utiliser votre fonction pour tracer 3 rectangles accolés de même largeur mais de hauteur différente.
 
