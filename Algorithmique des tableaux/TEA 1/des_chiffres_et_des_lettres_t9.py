9bc52924c305d2348267b57836827bc53695982d
Aslem1 <55717071+Aslem1@users.noreply.github.com>
2020-02-03 17:16:38 +0100

    Add files via upload

--- Algorithmique des tableaux/TEA 1/des_chiffres_et_des_lettres_t9.py
+++ Algorithmique des tableaux/TEA 1/des_chiffres_et_des_lettres_t9.py
@@ -1,11 +1,11 @@
 #Chiffrage T9
 conversion = {
-    "a" : 2,
-    "b":  2,
+    "a": 2,
+    "b": 2,
     "c": 2,
     "d": 3,
-    "e" : 3,
-    "f":  3,
+    "e": 3,
+    "f": 3,
     "g": 4,
     "h": 4,
     "i": 4,
@@ -25,11 +25,10 @@ conversion = {
     "w": 9,
     "x": 9,
     "y": 9,
-    "z": 9,
+    "z": 9
     }
 
-mot = "mon"
-
+mot = input()
 
 def chiffrage(mot):
     reponse = ""
@@ -37,29 +36,27 @@ def chiffrage(mot):
         reponse += str (conversion[letter])
         
     return reponse
-
 print (chiffrage(mot))
 
 
 #Déchiffrage T9
 conversionV2 = {
-    2: {"a","b","c"},
-    3: {"d","e","f"},
-    4: {"g","h","i"},
-    5: {"j","k","l"},
-    6: {"m","n","o"},
-    7: {"p","q","r","s"},
-    8: {"t","u","v"},
-    9: {"w","x","y","z"}
+    2: ["a","b","c"],
+    3: ["d","e","f"],
+    4: ["g","h","i"],
+    5: ["j","k","l"],
+    6: ["m","n","o"],
+    7: ["p","q","r","s"],
+    8: ["t","u","v"],
+    9: ["w","x","y","z"]
     }
 
-def dechiffrage (chiffres):
-    #charger dico dans variable
-    #compter nb de lettres dans var chiffre
-    #trouver mot possibles (de même taille)
+chiffres = input()
+
+def dechiffrage(chiffres):
+    reponse2 = "" 
     for ch in chiffres:
-        #trouver les mots dont la première lettre correspond au premier chiffre 
-        #recommencer pour chaque lettre
-    #elimination au fur et à mesure
-    #renvoyer tableau résultat
-print (dechiffrage ("666"))  #Doit renvoyer {"mon", "nom", "non"}
+        reponse2 += int (conversionV2[ch])
+    
+    return reponse2
+print (dechiffrage (chiffres))  #Doit renvoyer {"mon", "nom", "non"}
