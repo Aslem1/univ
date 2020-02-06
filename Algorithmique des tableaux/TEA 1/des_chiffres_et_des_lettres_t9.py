b64c318c006f1e79334b4eea875fec38b80a6a0b
Aslem <agathe_79@hotmail.fr>
2020-02-07 00:17:38 +0100

    rendu final TEA

--- Algorithmique des tableaux/TEA 1/des_chiffres_et_des_lettres_t9.py
+++ Algorithmique des tableaux/TEA 1/des_chiffres_et_des_lettres_t9.py
@@ -1,4 +1,6 @@
 #Chiffrage T9
+mot = input()
+## Exo n°1
 conversion = {
     "a": 2,
     "b": 2,
@@ -28,18 +30,19 @@ conversion = {
     "z": 9
     }
 
-mot = input()
-
 def chiffrage(mot):
     reponse = ""
     for letter in mot:
         reponse += str (conversion[letter])
         
     return reponse
+## Fin Exo n°1
 print (chiffrage(mot))
 
 
 #Déchiffrage T9
+chiffres = input()
+## Exo n°2
 conversionV2 = {
     2: ["a","b","c"],
     3: ["d","e","f"],
@@ -51,12 +54,62 @@ conversionV2 = {
     9: ["w","x","y","z"]
     }
 
-chiffres = input()
-
 def dechiffrage(chiffres):
-    reponse2 = "" 
-    for ch in chiffres:
-        reponse2 += int (conversionV2[ch])
+    with open("Dico.txt", "r") as f :  # ouvre le fichier -> cibler le bon fichier
+        lines = f.read().split('\n')
     
-    return reponse2
-print (dechiffrage (chiffres))  #Doit renvoyer {"mon", "nom", "non"}
+    # enlever de la liste tout ce qui n'a pas la bonne taille
+    new_lines = []
+    for line in lines:
+        if len(line) == len(chiffres):
+            new_lines.append(line)
+    
+    # pour chaque chiffre
+    for i in range(len(chiffres)):
+        # vérifier si les mots ont la bonne lettre à la bonne place
+        new_new_lines = [] # créer nouvelle nouvelle liste
+        for line in new_lines: # pour chaque mot de la nouvelle liste
+            if line[i] in conversionV2[int (chiffres[i])]: # vérifier que la lettre d'index n est dans la liste de lettres du chiffre
+                new_new_lines.append(line) # ajouter à la nouvelle nouvelle liste
+        new_lines = new_new_lines # mettre nouvelle nouvelle liste dans la nouvelle liste
+    if len(new_lines) == 0:
+        new_lines = "Aucun mot ne correspond à la recherche, désolé."
+    return new_lines
+## Fin Exo n°2
+print (dechiffrage(chiffres))
+
+
+
+#Déchiffrage T9
+chiffres = input()
+## Exo n°3
+conversionV2 = {
+    2: ["a","b","c"],
+    3: ["d","e","f"],
+    4: ["g","h","i"],
+    5: ["j","k","l"],
+    6: ["m","n","o"],
+    7: ["p","q","r","s"],
+    8: ["t","u","v"],
+    9: ["w","x","y","z"]
+    }
+
+def dechiffrage2(chiffres):
+    with open("Dico.txt", "r") as f :  # ouvre le fichier -> cibler le bon fichier
+        lines = f.read().split('\n')
+    
+    # pour chaque chiffre
+    for i in range(len(chiffres)):
+        # vérifier si les mots ont la bonne lettre à la bonne place
+        new_lines = [] # créer nouvelle liste
+        for line in lines: # pour chaque mot de la liste de mots
+            if i < len(line) and line[i] in conversionV2[int (chiffres[i])]: # vérifier que le mot en cours fait bien la bonne taille et vérifier que la lettre d'index n est dans la liste de lettres du chiffre
+                new_lines.append(line) # ajouter à la nouvelle liste
+        if len(new_lines) <= 5: # Si il reste 5 mots ou moins, renvoyer le nombre de chiffres parcourus et la liste associée.
+            return (i+1, new_lines)
+        else:
+            lines = new_lines # sinon, mettre nouvelle liste dans la liste de mots pour recommencer le traitement sur le chiffre suivant.
+    if len(lines) > 5: # Si il reste plus de 5 mots, renvoyer le tuple (None, None).
+        return (None, None)
+## Fin Exo n°3
+print (dechiffrage2(chiffres))
