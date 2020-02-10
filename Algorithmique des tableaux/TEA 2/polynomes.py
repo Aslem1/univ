72e498a0ece85ac0487fb383f6cb51e3aec689b6
Aslem1 <55717071+Aslem1@users.noreply.github.com>
2020-02-10 18:51:57 +0100

    Add files via upload

---
+++ Algorithmique des tableaux/TEA 2/polynomes.py
@@ -0,0 +1,7 @@
+def rndi_poly(n,min=-5,max=5):
+    import random
+    for n in random.randit (min,max):
+        tab = [] #creation tableau
+        for n in tab: #boucler n fois
+            tab.append(n) #ajouter valeur aleatoire tableau
+            print ("test"+ n) #afficher tableau
