9ab8cc57f5af7684405e4c9b341c200827502ba3
Aslem <agathe_79@hotmail.fr>
2020-02-25 21:32:25 +0100

    algo tableaux tea2

--- Algorithmique des tableaux/TEA 2/polynomes.py
+++ Algorithmique des tableaux/TEA 2/polynomes.py
@@ -10,28 +10,37 @@ def rndi_poly(n,min=-5,max=5):
 '''
 
 #Question 1 - Autre solution (qui fonctionne)
-'''
-def rndi_poly(n,min=-5,max=5):
+def rndi_poly(n, min=-5, max=5):
     import random
     tab = []
     for i in range (0, n+1):
-        a = random.randint(min,max)
+        a = random.randint(min, max)
         while a == 0 and i == n:
-           a = random.randint(min,max)
+           a = random.randint(min, max)
         tab.append(a)
     return tab
-'''
+print (rndi_poly(5))
 
 #Question 2
-def eval_poly(P,x):
+def eval_poly(P, x):
     somme = 0
     for i in range (len(P)):
         somme = somme + P[i]*x**i
     return somme
 
 #Question 3
-def eval_rec(P,x):
+def eval_rec(P, x):
+    if len(P) == 1:
+        return P[0]
+    return P[0] + x * eval_rec(P[1:], x)
+print(eval_rec([1, 2, 5, 6], 1))
+
+#Question 4
+def eval_iter(P, x):
     somme = 0
-    for i in range (a,len(P)):
-        somme = somme + P[-i]*x**i
+    while len(P) != 0:
+        somme += P[0]
+        P = [n*x for n in P[1:]]
     return somme
+l = [1, 2, 5, 6]
+print(eval_iter(l, 3))
