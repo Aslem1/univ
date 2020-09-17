0e8daf203bde15e148e1b86f0ab6b19d5f89c1b0
Aslem1 <55717071+Aslem1@users.noreply.github.com>
2020-09-17 12:27:45 +0200

    Add files via upload

--- TP1/Entreprise_LDD.sql
+++ TP1/Entreprise_LDD.sql
@@ -8,41 +8,40 @@ create table clients(
 	no_client integer
 );
 
-create table societe(
-	code_societe integer;
-	raison_sociale varchar(50);
-	adresse varchar(50);
-	ville varchar(50);
-	codepostal serial;
-	PRIMARY KEY(code_societe);
-	FOREIGN KEY () REFERENCES code_societe ()
+create table societe (
+	code_societe integer,
+	raison_sociale varchar(50) not null, --raison sociale = nom entreprise
+	adresse varchar(50) not null,
+	ville varchar(50) not null,
+	codepostal integer not null,
+	PRIMARY KEY(code_societe),
+	FOREIGN KEY (no_contrat) REFERENCES no_type (no_contrat, no_type)
 );
 
 create table contrat (
-	no_contrat integer;
-	date_expiration date;
-	duree time;
-	montant money;
-	societe text;
-	PRIMARY KEY(no_contrat);
-	FOREIGN KEY () REFERENCES no_contrat ()
+	no_contrat integer,
+	date_expiration date not null,
+	duree interval,
+	montant real not null,
+	societe integer not null,
+	PRIMARY KEY(no_contrat),
+	FOREIGN KEY (societe) REFERENCES societe (code_societe)
 );
 
 create table materiel (
-	no_materiel integer;
-	no_serie integer;
-	designation text;
-	no_contrat integer;
-	type text;
-	PRIMARY KEY(no_materiel);
-	FOREIGN KEY (no_contrat) REFERENCES no_materiel()
-	
+	no_materiel integer,
+	no_serie int not null,
+	designation varchar(50) not null,
+	no_contrat int not null,
+	type char not null,
+	PRIMARY KEY(no_materiel),
+	FOREIGN KEY (no_contrat) REFERENCES no_contrat(materiel)
 );
 
 create table type (
-	no_type integer;
-	designation text;
-	caracteristiques text;
-	PRIMARY KEY(no_type)
-	FOREIGN KEY () REFERENCES no_type ()
+	no_type integer,
+	designation varchar(50) not null,
+	caracteristiques text,
+	PRIMARY KEY(no_type),
+	FOREIGN KEY (carasteristiques) REFERENCES no_type (designation)
 );
