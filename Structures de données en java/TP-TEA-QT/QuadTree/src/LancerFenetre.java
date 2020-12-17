1668e6f12d400b830d8f88fa221971b3bfe972ed
Aslem <agathe_79@hotmail.fr>
2020-12-17 14:51:29 +0100

    TP-TEA-QT Java

---
+++ Structures de données en java/TP-TEA-QT/QuadTree/src/LancerFenetre.java
@@ -0,0 +1,9 @@
+class LancerFenetre {
+	
+ 
+ public static void main(String args[]) {
+ 	Fenetre maFenetre = new Fenetre("QuadTrees", 800, 800); 
+ 	
+ }
+ 
+}
---
+++ Structures de données en java/TP-TEA-QT/QuadTree/src/ReprUnique.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ReprUnique {
+	private ArrayList <ArbreQT> dico;
+	
+	public ReprUnique() {
+		this.dico = new ArrayList();
+	}	
+	
+	public ArbreQT getRef( ArbreQT a ) {
+		ArbreQT rep= new AQTVide();
+		
+      
+      
+      
+      
+      
+                return rep;
+	}
+}
+
