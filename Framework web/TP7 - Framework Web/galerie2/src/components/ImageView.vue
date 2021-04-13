1892663046ccc371360a011450538b1f131cceb0
Aslem <agathe.perrin@etudiant.univ-lr.fr>
2021-04-13 14:20:25 +0200

    Framework web - TP7

---
+++ Framework web/TP7 - Framework Web/galerie2/src/components/ImageView.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="photo">
+    <h2>{{ titre }}</h2>
+    <p><img :src="require('@/assets/' + image)" :alt="titre" /></p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ImageView",
+  props: {
+    image: String,
+    titre: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Framework web/TP7/galerie2/src/components/Galerie.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <ul>
+        <li v-for="p in this.photos" :key="p.mini">
+          <a href="#" :title="p.titre"><img :src="imageUrl(p.mini)" alt="p.titre" @mouseenter="changePhoto(p)"></a>
+        </li>
+    </ul>
+    <ImageView v-bind:image="image" v-bind:titre="titre"></ImageView>
+  </section>
+</template>
+
+<script>
+import ImageView from "./ImageView"
+export default {
+  name: "Galerie",
+  props: {
+    photos: Array
+  },
+  components : {
+    ImageView
+  },
+  methods: {
+    changePhoto(p) {
+      this.image=p.lien
+      this.titre=p.titre
+    },
+    imageUrl(p) {
+      // permet de placer les images dans le dossier assets et que cela fonctionne qu’elle que soit la route…
+      return require('@/assets/' + p)
+    }
+  },
+  data() {
+    return {
+      image: 'images/photo1.jpg',
+      titre: 'Pieuvre',
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
