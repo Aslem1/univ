4204453664a1a990f71aa6aa77c98688c9f87fa1
Aslem <agathe.perrin@etudiant.univ-lr.fr>
2021-05-03 12:06:31 +0200

    Framework web - TP9

---
+++ Framework web/TP9/meteo/src/components/Meteo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="meteo">
+    <a href="#" :title="ville" @click="getData(ville)">{{ville}}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Meteo',
+  props: {
+    ville: String
+  },
+  methods : {
+    getData(v) {
+      alert(this.$store.state.urlMeteo)
+      alert(v)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Framework web/TP9/meteo/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Meteo :ville="ville1"/>
+    <Meteo :ville="ville2"/>
+    <Meteo ville="Axel je t'aime plus que tout"/>
+    <Meteo ville="465634"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Meteo from '@/components/Meteo.vue'
+
+export default {
+  name: 'Home',
+  components : {
+    Meteo
+  },
+  data() {
+    return {
+      // Ville 1
+      ville1 : 'La Rochelle',
+      countryCode1 : '33',
+
+      // Ville 2
+      ville2 : 'Rochefort'
+    }
+  },
+
+
+}
+</script>
